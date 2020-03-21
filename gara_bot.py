dabf4c34bbe9488d84d7039ee4a49fa5cd06940f
hajakuja <uqabra@gmail.com>
2020-03-21 21:35:27 +0300

    added entries dates to the data

--- gara_bot.py
+++ gara_bot.py
@@ -17,31 +17,31 @@ def initial(message):
     bot.reply_to(message, "Hi!")
 
 
-#
-#
+
+
 # @bot.message_handler(func=lambda m: True)
 # def echo_all(message):
 #     bot.reply_to(message, message.text)
 # bot.polling()  
-# 
-#       
+
+      
 
 
 @bot.message_handler(func=lambda m: m.text in settings.ACC_MESSAGES)
 def count_save(message):
     who = message.from_user
-    # data_string = ''
-    # with open("data/data.json",'r') as data_file:
-    #     data_string = data_file.read()
-    # print("The original json : {}\n".format(data_string))
+    
     f = open("data/data.json",'r')
     data_json = json.load(f)
     f.close()
-    print("The original json data type : {}\n".format(type(data_json)))
-    if who.first_name in data_json:
-        data_json[who.first_name] += 1
+    
+    # print("The original json data type : {}\n".format(type(data_json)))
+    
+    if str(who.id) in data_json:
+        data_json[str(who.id)]['count'] += 1
+        data_json[str(who.id)]['count'].append(str(datetime.now()))
     else:
-        data_json[who.first_name] = 1
+        data_json[who.id] = {'name':who.first_name, 'count':1, 'entry_dates':[str(datetime.now())]}
 
     with open('data/data.json', 'w') as data_file:
         json.dump(data_json, data_file)
